abcddb985ba9ecb80235c4afc9d82ef4752f1bf6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-09-12 08:51:41 -0400

    added shots and initial files

---
+++ fusion_toolbox/shot_analysis.py
@@ -0,0 +1,37 @@
+def smooth(data):
+    """Smooth the data to remove noise
+
+    Args:
+        data (_type_): _description_
+    """
+    pass
+
+def compute_duration_phases(shot):
+    """Determins the duration of the ramp-up, flat-top and ramp down phases
+    """
+    pass
+
+def compute_flat_top_plasma_temperature(shot):
+    pass
+
+def compute_flat_top_plasma_current(shot):
+    pass
+
+def compute_flat_top_plasma_density(shot):
+    pass
+
+def find_shots_from_fusion_power(fusion_power, rtol):
+    """Finds shots in the database with the desired fusion power
+
+    Args:
+        fusion_power (float): the desired fusion power
+        rtol (float): the relative tolerance
+    """
+    pass
+
+def compute_average_fusion_power(shots):
+    """Computes the average fusion power over some shots
+
+    Args:
+        shots (list): shot numbers
+    """
