e1d029e21172cdcc4147e5563b013a36f6dea3e2
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-09-12 12:13:06 -0400

    refactored

--- fusion_toolbox/plot.py
+++ fusion_toolbox/plot.py
@@ -8,7 +8,7 @@ def plot_shot(shot_number):
                         names=True, dtype=None, encoding=None)
 
     # Extract data columns
-    time_points = data['Time']
+    time_points = data['Time_s']
     plasma_current_points = data['Plasma_Current_A']
     plasma_density_points = data['Plasma_Density_particlesm3']
     plasma_temperature_points = data['Plasma_Temperature_K']
@@ -25,23 +25,23 @@ def plot_shot(shot_number):
     fig.suptitle('Fusion Plasma Data Over Time', fontsize=16)
 
     # Subplot 1: Plasma Current
-    axs[0].plot(time_points, plasma_current_points, label='Plasma Current (A)', color='tab:blue')
+    axs[0].plot(time_points, plasma_current_points, label='Plasma Current (A)')
     axs[0].set_ylabel('Plasma Current (A)')
     axs[0].legend(loc='upper left')
 
     # Subplot 2: Plasma Density
-    axs[1].plot(time_points, plasma_density_points, label='Plasma Density (particles/m^3)', color='tab:orange')
+    axs[1].plot(time_points, plasma_density_points, label='Plasma Density (particles/m^3)')
     axs[1].set_ylabel('Plasma Density (particles/m^3)')
     axs[1].legend(loc='upper left')
 
     # Subplot 3: Plasma Temperature
-    axs[2].plot(time_points, plasma_temperature_points, label='Plasma Temperature (K)', color='tab:green')
+    axs[2].plot(time_points, plasma_temperature_points, label='Plasma Temperature (K)')
     axs[2].set_ylabel('Plasma Temperature (K)')
     axs[2].legend(loc='upper left')
 
     # Subplot 4: Fusion Power and Radiation Power
-    axs[3].plot(time_points, fusion_power_points, label='Fusion Power (W)', color='tab:red')
-    axs[3].plot(time_points, radiation_power_points, label='Radiation Power (W)', linestyle='--', color='tab:purple')
+    axs[3].plot(time_points, fusion_power_points, label='Fusion Power (W)')
+    axs[3].plot(time_points, radiation_power_points, label='Radiation Power (W)')
     axs[3].set_ylabel('Power (W)')
     axs[3].legend(loc='upper left')
 
